5c9c77c43d5a2222349b75faa2a35534e733d004
saimun212 <saimunislam00402@gmail.com>
2025-08-17 21:24:45 +0600

    add our comment

---
+++ math.js
@@ -0,0 +1,30 @@
+/* 
+****************************** *************************************************
+*                           MATH NEEDED FOR PROGRAMMING
+*********************************************************************************
+* 1.Number  [   positive (+ve), Negative (-ve), (0) ]
+            example: [50, positive], [-9 Negative],[0]
+
+* 2.Integer : to type math : { odd, even = odd mane bijore and even mane jor}    
+              example:[2,88 etc]  odd = 597 even = 598
+
+* 3. Float/ Faction : example: [5.6, 7.5 etc]   
+
+* 4. Operation :i. (+)add / joke 
+               ii. (-)negative/ biog
+              iii. (*) gune
+               iv. (/) vag
+
+* 5. More Operation : (%) [ ] vag ses ber korar jonne .
+* 6. percentage : 5% ba 50 taka ar 5% ba 10%  koto hobe ata bujte hobe 
+* 7. Sum: 14, 15,16  somosti 
+* 8. Avg: 14, 15,16 gor
+* 9. Max/ Min: 14, 15,16 ader modde konta bro ba choto number.
+* 10.Series : 1-10  amon kishue 1 theke 10 projonto ba tar o besi jante chaite pare 
+* 11. Area:1 trivug her Triangle  ba chotur vug her four side 1/2 haf vumi intue uchatta ;
+*
+*/
+
+
+
+
